3faa02dbfe72ef34b67eb2fd772d6f80031e7400
Seymour James <seymourpjames@gmail.com>
2022-07-09 04:23:44 -0700

    search router

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,11 @@
 import express, { Response, Request, Application } from "express";
 import "dotenv/config";
+import searchRouter from "./routes/search";
 
 const app: Application = express();
 
-const port = process.env.PORT || 3000;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("hello");
-});
+const port: string | number = process.env.PORT || 3000;
+app.use("/api/v1", searchRouter);
 
 app.listen(port, () =>
   console.log(`Server is running on http://localhost:${port}`)
---
+++ src/routes/search.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response, IRouter } from "express";
+const axios = require("axios").default;
+
+const router: IRouter = express.Router();
+
+const key: string | undefined = process.env.KEY;
+const host: string | undefined = process.env.HOST;
+const searchUrl: string | undefined = process.env.SEARCH_URL;
+
+router.get("/search", async (req: Request, res: Response) => {
+  const { data } = await axios.get(searchUrl, {
+    params: { location: req.query.location, home_type: req.query.home_type },
+    headers: {
+      "X-RapidAPI-Key": key,
+      "X-RapidAPI-Host": host,
+    },
+  });
+  res.json(data.props);
+});
+
+export = router;
